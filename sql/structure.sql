803378e88507ec8f8f705454f9238ff7c4531bd5
Sanpi <sanpi@homecomputing.fr>
2016-08-01 17:40:29 +0200

    Create table if not exists

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,16 +1,16 @@
-CREATE TABLE room (
+CREATE TABLE IF NOT EXISTS room (
     room_id SERIAL PRIMARY KEY,
     label CHARACTER VARYING NOT NULL
 );
 
-CREATE TABLE temperature (
+CREATE TABLE IF NOT EXISTS temperature (
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     room_id INTEGER REFERENCES room(room_id),
     temperature NUMERIC NOT NULL,
     PRIMARY KEY (created, room_id)
 );
 
-CREATE TABLE humidity (
+CREATE TABLE IF NOT EXISTS humidity (
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     room_id INTEGER REFERENCES room(room_id),
     humidity NUMERIC NOT NULL,
